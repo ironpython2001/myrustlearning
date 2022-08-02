f38256b42c98d552979c5e2e05b6044f8e5ff9ed
Sunil Pottumuttu <spottumuttu@osius.com>
2022-08-02 19:45:24 +0530

    1

---
+++ chap2a/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let x: i32 = 5;
+    println!("value of x is {}",x);
+}
---
+++ variablemutability/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let mut x: i32 = 5;
+    println!("value of x is {}", x);
+    x = 10;
+    println!("value of x is {}", x);
+}
