dc7ebccd72310c1b2bd06834ad2b2620b1d60420
Sunil Pottumuttu <spottumuttu@osius.com>
2022-08-02 21:57:01 +0530

    1

--- datatype1/src/main.rs
+++ datatype1/src/main.rs
@@ -1,5 +1,7 @@
+
 fn main() {
-    let y:i32 = "10".parse().unwrap();
+    let y: i32 = "10".parse().unwrap();
+    let x: String = "asdffasf".to_string();
     println!("{}", y);
-   
+    println!("{}", x);
 }
